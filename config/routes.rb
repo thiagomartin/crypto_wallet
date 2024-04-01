5c57786d7b8dbe39fefda4c36169c574de107085
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-04-01 08:37:15 -0300

    feat routes welcome

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :clients
   resources :coins
+
+
+  root  "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

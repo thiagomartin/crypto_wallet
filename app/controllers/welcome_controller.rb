df2c678228862f68aacaf541f9457e0bad1bfde3
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-04-10 11:11:24 -0300

    learn cookies/session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails [SESSION]"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
